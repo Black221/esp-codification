9032dd8a0958e4aae93521d9ec8ca4a7e18042c8
Black221 <bambabar02@gmail.com>
2022-12-21 01:40:17 +0000

    new version

--- src/components/Admin/TableBottom.jsx
+++ src/components/Admin/TableBottom.jsx
@@ -61,7 +61,7 @@ export default function TableBottom({ resetOrder, resetFilters, table }) {
                     onChange={(e) => {
                         table.setPageSize(Number(e.target.value));
                     }}>
-                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                    {[10, 20, 30, 40, 50, 100, 200].map((pageSize) => (
                         <option key={pageSize} value={pageSize}>
                             Show {pageSize}
                         </option>
